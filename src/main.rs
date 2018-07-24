d9fb2683d961a7a7c011a67281f937f4cee1222b
Tim Bruijnzeels <tim@nlnetlabs.nl>
2018-07-24 16:23:04 +0200

    Healthcheck on /health and 404 elsewhere

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,42 @@
 extern crate rpki_publication_server;
 
 extern crate hyper;
+extern crate futures;
 
-use hyper::{Body, Request, Response, Server};
+use futures::future;
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::rt::Future;
-use hyper::service::service_fn_ok;
-use std::net::SocketAddr;
+use hyper::service::service_fn;
 
-fn service_ok(_req: Request<Body>) -> Response<Body> {
-    Response::new(Body::from(
-        "I am completely operational, and all my circuits are functioning perfectly."))
+type BoxFut = Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>;
+
+fn service_ok() -> Body {
+    Body::from("I am completely operational, and all my circuits are functioning perfectly.")
+}
+
+fn request_mapper(req: Request<Body>) -> BoxFut {
+
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/health") => {
+            *response.body_mut() = { service_ok() }
+        },
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+            *response.body_mut() = Body::from("404 - These are not the droids you're looking for.")
+        },
+    };
+
+    Box::new(future::ok(response))
+
 }
 
 fn main() {
-    let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
-
-    let health_svc = || {
-        // service_fn_ok converts our function into a `Service`
-        service_fn_ok(service_ok)
-    };
+    let addr = ([127, 0, 0, 1], 3000).into();
 
     let server = Server::bind(&addr)
-        .serve(health_svc)
+        .serve(|| service_fn(request_mapper))
         .map_err(|e| eprintln!("server error: {}", e));
 
     // Run this server for... forever!
