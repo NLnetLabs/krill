fb6747a313c541627f8f76ddbbcd34291eddc221
Tim Bruijnzeels <tim@nlnetlabs.nl>
2018-07-24 15:17:16 +0200

    Run web server on hardcoded ip and port 127.0.0.1:3000 and return ok

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 extern crate rpki_publication_server;
 
+extern crate hyper;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use std::net::SocketAddr;
+
+fn service_ok(_req: Request<Body>) -> Response<Body> {
+    Response::new(Body::from(
+        "I am completely operational, and all my circuits are functioning perfectly."))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
+
+    let health_svc = || {
+        // service_fn_ok converts our function into a `Service`
+        service_fn_ok(service_ok)
+    };
+
+    let server = Server::bind(&addr)
+        .serve(health_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    // Run this server for... forever!
+    hyper::rt::run(server);
 }
