cb61eba5cd77221bc10bd49742b3e28ca445d2a2
Tim Bruijnzeels <tim@nlnetlabs.nl>
2018-07-25 11:23:43 +0200

    Introduced house style 404 page

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate rpki_publication_server;
 extern crate hyper;
 extern crate futures;
 
+use std::fs;
+
 use futures::future;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::rt::Future;
@@ -14,19 +16,33 @@ fn service_ok() -> Body {
     Body::from("I am completely operational, and all my circuits are functioning perfectly.")
 }
 
+fn read_static_file(path: &str) -> Body {
+    match path {
+        "/static/css/custom.css" => { Body::from(fs::read("static/css/custom.css").unwrap()) },
+        "/static/images/404.png" => { Body::from(fs::read("static/images/404.png").unwrap()) },
+        "/static/html/404.html" => { Body::from(fs::read("static/html/404.html").unwrap()) },
+        _ => { Body::from("Unknown resource") },
+    }
+}
+
 fn request_mapper(req: Request<Body>) -> BoxFut {
 
     let mut response = Response::new(Body::empty());
+    let path = req.uri().path();
 
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/health") => {
-            *response.body_mut() = { service_ok() }
-        },
-        _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
-            *response.body_mut() = Body::from("404 - These are not the droids you're looking for.")
-        },
-    };
+    if path.starts_with("/static") {
+        *response.body_mut() = read_static_file(path)
+    } else {
+        match (req.method(), path) {
+            (&Method::GET, "/health") => {
+                *response.body_mut() = { service_ok() }
+            },
+            _ => {
+                *response.status_mut() = StatusCode::NOT_FOUND;
+                *response.body_mut() = read_static_file("/static/html/404.html")
+            },
+        };
+    }
 
     Box::new(future::ok(response))
 
