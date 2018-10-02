bd7754746555551026fc31218fc8167f8bd6d445
Tim Bruijnzeels <tim@nlnetlabs.nl>
2018-10-02 16:01:51 +0200

    Using a PathBuf in the Key instead of a String. Also checking validatity of path against RFC 3896.

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -6,11 +6,12 @@ use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::Write;
-use std::path::Path;
 use std::sync::Arc;
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 use serde_json;
+use std::path::PathBuf;
+use std::path::Component;
 
 /// A Key for KeyStores.
 ///
@@ -18,20 +19,50 @@ use serde_json;
 /// be used to map keys to file a on disk for storage.
 #[derive(Clone, Debug, Hash, Eq, PartialEq)]
 pub struct Key {
-    path: String
+    path: PathBuf
 }
 
 impl Key {
-    /// Creates a new key based on the path. Only the following characters are
-    /// allowed: alphanumeric and '/'. Paths MAY NOT start with a '/' - they
-    /// are supposed to be relative. The '.' character is disallowed to
-    /// prevent that relative paths go outside of their predesignated scope.
+    /// Creates a new key based on the path.
+    ///
+    /// Paths must be relative, must not contain '\' (but '/' will work on
+    /// windows here), must not contain '/..' to avoid escaping the base_dir
+    /// for the DiskKeyStore implementation. And may contain characters
+    /// allowed in the 'hier-part' defined in RFC3896 only, i.e:
+    ///
+    /// path-rootless = segment-nz *( "/" segment )
+    ///
+    /// segment       = *pchar
+    /// segment-nz    = 1*pchar
+    ///
+    /// pchar         = unreserved / pct-encoded / sub-delims / ":" / "@"
+    ///
+    /// unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
+    /// pct-encoded   = "%" HEXDIG HEXDIG
+    /// sub-delims    = "!" / "$" / "&" / "'" / "(" / ")" /
+    ///                 "*" / "+" / "," / ";" / "="
     pub fn new(path: String) -> Result<Key, InvalidKey> {
-        if Path::new(&path).is_relative() {
-            Ok(Key{path})
-        } else {
-            Err(InvalidKey)
+
+        if ! path.bytes().all(|b| {
+            b == b'/' || // Allow sub-dirs
+            b.is_ascii_alphanumeric() || // ALPHA DIGIT
+            b == b'-' || b == b'.' || b == b'_' || b == b'~' ||
+            b == b'%' || // Not checking against invalid % encoding (e.g. %%)
+            b == b'!' || b == b'$' || b == b'&' || b == b'\'' ||
+            b == b'(' || b == b')' || b == b'*' || b == b'+'  ||
+            b == b',' || b == b';' || b == b'='
+        }) {
+            return Err(InvalidKey)
         }
+
+        let path = PathBuf::from(path);
+        if path.components().all(|c| { c == Component::Normal("..".as_ref())})
+           || ! path.is_relative()
+        {
+            return Err(InvalidKey);
+        }
+
+        Ok(Key{path})
     }
 }
 
@@ -111,13 +142,14 @@ impl KeyStore for MemoryKeyStore {
 
 #[derive(Debug)]
 pub struct DiskKeyStore {
-    base_dir: String
+    base_dir: PathBuf
 }
 
 impl DiskKeyStore {
     pub fn new(base_dir: String) -> Result<Self, Error> {
         let meta_data = fs::metadata(&base_dir)?;
         if meta_data.is_dir() {
+            let base_dir = PathBuf::from(base_dir);
             Ok(DiskKeyStore{base_dir})
         } else {
             panic!("Invalid base_dir for DiskKeyStore")
@@ -133,8 +165,10 @@ impl KeyStore for DiskKeyStore {
         value: V
     ) -> Result<(), Error> {
         let v = serde_json::to_string(&value)?;
-        let path_string = format!("{}{}", self.base_dir, key.path);
-        let full_path = Path::new(&path_string);
+
+        let mut full_path = PathBuf::new();
+        full_path.push(self.base_dir.as_path());
+        full_path.push(key.path.as_path());
 
         if !full_path.exists() {
             if let Some(base_path) = full_path.parent() {
@@ -152,12 +186,14 @@ impl KeyStore for DiskKeyStore {
         &self,
         key: &Key
     ) -> Result<Option<Arc<V>>, Error> {
-        let path_string = format!("{}{}", self.base_dir, key.path);
-        let path = Path::new(&path_string);
-        if ! path.exists() {
+        let mut full_path = PathBuf::new();
+        full_path.push(self.base_dir.as_path());
+        full_path.push(key.path.as_path());
+
+        if ! full_path.exists() {
             Ok(None)
         } else {
-            let f = File::open(path)?;
+            let f = File::open(full_path)?;
             let v: V = serde_json::from_reader(f)?;
             Ok(Some(Arc::new(v)))
         }
