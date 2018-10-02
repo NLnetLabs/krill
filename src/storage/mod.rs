120c123eb66ef0c1074d68de6522cfea98510dc6
Tim Bruijnzeels <tim@nlnetlabs.nl>
2018-10-02 14:02:15 +0200

    Adding disk based keystore.

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,7 +1,15 @@
 //! The KeyStore trait and some implementations.
 
+use std::any::Any;
 use std::collections::HashMap;
-use serde::{Deserialize, Serialize};
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::Write;
+use std::path::Path;
+use std::sync::Arc;
+use serde::Serialize;
+use serde::de::DeserializeOwned;
 use serde_json;
 
 /// A Key for KeyStores.
@@ -19,9 +27,7 @@ impl Key {
     /// are supposed to be relative. The '.' character is disallowed to
     /// prevent that relative paths go outside of their predesignated scope.
     pub fn new(path: String) -> Result<Key, InvalidKey> {
-        if path.bytes().all(
-            |b| b.is_ascii_alphanumeric() || b == b'/' || b == b'.'
-        ) {
+        if Path::new(&path).is_relative() {
             Ok(Key{path})
         } else {
             Err(InvalidKey)
@@ -33,15 +39,30 @@ impl Key {
 pub struct InvalidKey;
 
 pub trait KeyStore {
-    fn store<V: Serialize>(&mut self, key: Key, value: V) -> Result<(), Error>;
-    fn retrieve<'a, V: Deserialize<'a>>(&'a self, key: &Key) -> Result<Option<V>, Error>;
+    /// Stores a key value pair.
+    fn store<V: Serialize + Any>(
+        &mut self,
+        key: Key,
+        value: V
+    ) -> Result<(), Error>;
+
+    /// Retrieves an optional reference to a value, given the key.
+    fn retrieve<V: Any + Clone + DeserializeOwned>(
+        &self,
+        key: &Key
+    ) -> Result<Option<Arc<V>>, Error>;
 }
 
 #[derive(Debug, Fail)]
 pub enum Error {
-
     #[fail(display ="Json serialization error: {}", _0)]
-    JsonError(serde_json::Error)
+    JsonError(serde_json::Error),
+
+    #[fail(display ="Something went wrong: {}", _0)]
+    IoError(io::Error),
+
+    #[fail(display ="Something went wrong: {}", _0)]
+    Other(String)
 }
 
 impl From<serde_json::Error> for Error {
@@ -50,10 +71,16 @@ impl From<serde_json::Error> for Error {
     }
 }
 
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Error::IoError(e)
+    }
+}
 
 
+#[derive(Debug)]
 pub struct MemoryKeyStore {
-    store: HashMap<Key, String>
+    store: HashMap<Key, Box<Any>>
 }
 
 impl MemoryKeyStore {
@@ -63,22 +90,81 @@ impl MemoryKeyStore {
 }
 
 impl KeyStore for MemoryKeyStore {
-    fn store<V: Serialize>(&mut self, key: Key, value: V) -> Result<(), Error> {
-        let v = serde_json::to_string(&value)?;
+    fn store<V: Serialize + Any>(&mut self, key: Key, value: V) -> Result<(), Error> {
+        let v = Box::new(value);
         self.store.entry(key).or_insert(v);
         Ok(())
     }
 
-    fn retrieve<'a, V: Deserialize<'a>>(&'a self, key: &Key) -> Result<Option<V>, Error> {
+    fn retrieve<V: Any + Clone + DeserializeOwned>(&self, key: &Key) -> Result<Option<Arc<V>>, Error> {
         match self.store.get(key) {
             None => Ok(None),
-            Some(v) => {
-                let res: V = serde_json::from_str(v.as_ref())?;
-                Ok(Some(res))
+            Some(ref v) => {
+                match v.downcast_ref::<V>() {
+                    Some(res) => Ok(Some(Arc::new(res.clone()))),
+                    None => Err(Error::Other("Object has the wrong type!".to_string()))
+                }
             }
         }
     }
 }
+
+#[derive(Debug)]
+pub struct DiskKeyStore {
+    base_dir: String
+}
+
+impl DiskKeyStore {
+    pub fn new(base_dir: String) -> Result<Self, Error> {
+        let meta_data = fs::metadata(&base_dir)?;
+        if meta_data.is_dir() {
+            Ok(DiskKeyStore{base_dir})
+        } else {
+            panic!("Invalid base_dir for DiskKeyStore")
+        }
+    }
+}
+
+impl KeyStore for DiskKeyStore {
+
+    fn store<V: Serialize + Any>(
+        &mut self,
+        key: Key,
+        value: V
+    ) -> Result<(), Error> {
+        let v = serde_json::to_string(&value)?;
+        let path_string = format!("{}{}", self.base_dir, key.path);
+        let full_path = Path::new(&path_string);
+
+        if !full_path.exists() {
+            if let Some(base_path) = full_path.parent() {
+                fs::create_dir_all(base_path)?;
+            }
+        }
+
+        let mut f = File::create(full_path)?;
+        f.write(v.as_ref())?;
+
+        Ok(())
+    }
+
+    fn retrieve<V: Any + Clone + DeserializeOwned>(
+        &self,
+        key: &Key
+    ) -> Result<Option<Arc<V>>, Error> {
+        let path_string = format!("{}{}", self.base_dir, key.path);
+        let path = Path::new(&path_string);
+        if ! path.exists() {
+            Ok(None)
+        } else {
+            let f = File::open(path)?;
+            let v: V = serde_json::from_reader(f)?;
+            Ok(Some(Arc::new(v)))
+        }
+    }
+}
+
+
 
 
 //------------ Tests ---------------------------------------------------------
@@ -95,14 +181,28 @@ mod tests {
     }
 
     #[test]
-    fn should_store_and_retrieve() {
+    fn should_store_and_retrieve_in_memory() {
         let mut store = MemoryKeyStore::new();
         let key = Key::new("some/path/file.txt".to_string()).unwrap();
         let value = TestStruct { v1: "blabla".to_string(), v2: 42 };
         store.store(key.clone(), value.clone()).unwrap();
 
-        let found: Option<TestStruct> = store.retrieve(&key).unwrap();
+        let found: Option<Arc<TestStruct>> = store.retrieve(&key).unwrap();
 
-        assert_eq!(Some(value), found)
+        assert_eq!(Some(Arc::new(value)), found)
     }
+
+    #[test]
+    fn should_store_and_retrieve_from_disk() {
+        let mut store = DiskKeyStore::new("work/".to_string()).unwrap();
+        let key = Key::new("some/path/file.txt".to_string()).unwrap();
+        let value = TestStruct { v1: "blabla".to_string(), v2: 42 };
+        store.store(key.clone(), value.clone()).unwrap();
+
+        let found: Option<Arc<TestStruct>> = store.retrieve(&key).unwrap();
+
+        assert_eq!(Some(Arc::new(value)), found)
+    }
+
+
 }
