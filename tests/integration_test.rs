3e2aaa93e514b37b718ce3cd420dd64dff54eff9
Tim Bruijnzeels <tim@nlnetlabs.nl>
2018-10-29 14:41:32 +0100

    Fixing warnings for integration test, leaving it as a work in progress.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -9,9 +9,6 @@ use std::path::PathBuf;
 use std::str::FromStr;
 use rpki::oob::exchange::PublisherRequest;
 use rpubd::test;
-use rpubd::server;
-use std::time::Duration;
-use hyper::server::conn::Serve;
 
 fn save_pr(base_dir: &str, file_name: &str, pr: &PublisherRequest) {
     let full_name = PathBuf::from(format!("{}/{}", base_dir, file_name));
@@ -25,7 +22,7 @@ fn testing() {
     test::test_with_tmp_dir(|d| {
 
         // Use a data dir for the storage
-        let data_dir = test::create_sub_dir(&d);
+        let _data_dir = test::create_sub_dir(&d);
 
         // Start with an xml dir with two PRs for alice and bob
         let xml_dir = test::create_sub_dir(&d);
@@ -34,9 +31,10 @@ fn testing() {
         save_pr(&xml_dir, "alice.xml", &pr_alice);
         save_pr(&xml_dir, "bob.xml", &pr_bob);
 
-        let addr = SocketAddr::from_str("127.0.0.1:3000").unwrap();
+        let _addr = SocketAddr::from_str("127.0.0.1:3000").unwrap();
 
-        unimplemented!()
+        // TODO Run the server, and tear it down at the end of the test
+
 
 
     });
