4adc5444a9fcef485c2cc3f2260d10b36faccc96
Tim Bruijnzeels <tim@nlnetlabs.nl>
2018-11-12 12:49:05 +0100

    Introducing binary for CLI to publication server.

---
+++ src/bin/pubc.rs
@@ -0,0 +1,10 @@
+//! Command line client to the publication server.
+//!
+//! Can be used for testing the publication server, but may also be useful
+//! for setups where a CA simply writes its current state to some disk, so
+//! that this CLI may be triggered to synchronise this state to a publication
+//! server.
+
+fn main() {
+    println!("Hello world");
+}
