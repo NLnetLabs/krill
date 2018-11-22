ec8e0bb2152a39f5675b09f1d6d8c513c1ad48d1
Tim Bruijnzeels <tim@nlnetlabs.nl>
2018-11-22 16:56:57 +0100

    Support processing a publish query (containing publish/update/withdraw elements) in the Repository and RepoPublisher.

--- src/repo/repository.rs
+++ src/repo/repository.rs
@@ -5,8 +5,14 @@ use std::sync::Arc;
 use bytes::Bytes;
 use ext_serde;
 use rpki::uri;
+use rpki::publication;
 use storage::caching_ks::CachingDiskKeyStore;
 use storage::keystore::{ self, Info, Key, KeyStore };
+use rpki::publication::query::PublishQuery;
+use rpki::publication::query::PublishElement;
+use rpki::publication::query::Update;
+use rpki::publication::query::Publish;
+use rpki::publication::query::Withdraw;
 
 
 /// # Naming things in the keystore.
@@ -78,8 +84,41 @@ impl Repository {
 
         Ok(res)
     }
+
+    pub fn get_publisher(
+        &self,
+        publisher_handle: &str
+    ) -> Result<Arc<RepoPublisher>, Error> {
+        let key = Key::from_str(publisher_handle);
+        if let Some(repo_publisher) = self.store.get(&key)? {
+            Ok(repo_publisher)
+        } else {
+            Err(Error::UnknownPublisher(publisher_handle.to_string()))
+        }
+    }
 }
 
+/// # List / Publish / Withdraw
+///
+impl Repository {
+    pub fn publish(
+        &mut self,
+        publisher_handle: &str,
+        query: PublishQuery
+    ) -> Result<(), Error> {
+        let rp = self.get_publisher(publisher_handle)?;
+        let updated_rp = rp.publish(query)?;
+        let key = Key::from_str(&rp.publisher_handle);
+        let inf = Info::now(
+            actor(),
+            "Updated files".to_string()
+        );
+        self.store.store(key, updated_rp, inf)?;
+        Ok(())
+    }
+
+
+}
 
 //------------ RepoPublisher -------------------------------------------------
 
@@ -104,6 +143,99 @@ impl RepoPublisher {
             files
         }
     }
+
+    pub fn current_files(&self) -> &Vec<CurrentFile> {
+        &self.files
+    }
+
+    fn process_update(
+        list: &mut Vec<CurrentFile>,
+        update: &Update
+    ) -> Result<(), Error> {
+        if let Some(entry) = list.iter().find(
+            |f| {&f.uri == update.uri() }
+        ) {
+            if entry.hash != update.hash() {
+                return Err(Error::UpdateWrongHash)
+            }
+        } else {
+            return Err(Error::UpdateWrongUri(update.uri().clone()))
+        }
+
+        list.retain(|e| { &e.uri != update.uri() });
+        let new_entry = CurrentFile::new(
+            update.uri().clone(),
+            update.object().clone()
+        );
+        list.push(new_entry);
+        Ok(())
+
+    }
+
+    fn process_publish(
+        list: &mut Vec<CurrentFile>,
+        publish: &Publish
+    ) -> Result<(), Error> {
+        if let Some(entry) = list.iter().find(
+            |f| {&f.uri == publish.uri() }
+        ) {
+            return Err(Error::PublishHasExistingUri(entry.uri.clone()))
+        }
+        let new_entry = CurrentFile::new(
+            publish.uri().clone(),
+            publish.object().clone()
+        );
+        list.push(new_entry);
+        Ok(())
+    }
+
+    fn process_withdraw(
+        list: &mut Vec<CurrentFile>,
+        withdraw: &Withdraw
+    ) -> Result<(), Error> {
+        if let Some(entry) = list.iter().find(
+            |f| {&f.uri == withdraw.uri() }
+        ) {
+            if entry.hash != withdraw.hash() {
+                return Err(Error::WithdrawWrongHash)
+            }
+        } else {
+            return Err(Error::WithdrawWrongUri(withdraw.uri().clone()))
+        }
+
+        list.retain(|e| { &e.uri != withdraw.uri() });
+        Ok(())
+    }
+
+    /// Returns a new RepoPublisher with an updated list of files.
+    ///
+    /// Note that RepoPublishers are stored as versioned immutable (Arc)
+    /// in the keystore, and therefore a new instance is needed whenever
+    /// there is a change.
+    pub fn publish(&self, query: PublishQuery) -> Result<Self, Error> {
+
+        let mut new_list = self.files.clone();
+
+        for el in query.elements() {
+            match el {
+                PublishElement::Publish(p) => {
+                    Self::process_publish(&mut new_list, p)?;
+                },
+                PublishElement::Update(u) => {
+                    Self::process_update(&mut new_list, u)?;
+                },
+                PublishElement::Withdraw(w) => {
+                    Self::process_withdraw(&mut new_list, w)?;
+                },
+            }
+        }
+
+        Ok(RepoPublisher {
+            publisher_handle: self.publisher_handle.clone(),
+            base_uri: self.base_uri.clone(),
+            files: new_list
+        })
+    }
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -116,8 +248,60 @@ pub struct CurrentFile {
     #[serde(
     deserialize_with = "ext_serde::de_bytes",
     serialize_with = "ext_serde::ser_bytes")]
-    content: Bytes
+    /// The actual file content. Note that we may want to store this
+    /// only on disk in future (look up by sha256 hash), to save memory.
+    content: Bytes,
+
+    #[serde(
+    deserialize_with = "ext_serde::de_bytes",
+    serialize_with = "ext_serde::ser_bytes")]
+    /// The sha-256 hash of the file (as is used on the RPKI manifests and
+    /// in the publication protocol for list, update and withdraw). Saving
+    /// this rather than calculating on demand seems a small price for some
+    /// performance gain.
+    hash:    Bytes
 }
+
+impl CurrentFile {
+    pub fn new(uri: uri::Rsync, content: Bytes) -> Self {
+        let hash = publication::hash(&content);
+        CurrentFile {uri, content, hash}
+    }
+
+    pub fn uri(&self) -> &uri::Rsync {
+        &self.uri
+    }
+
+    pub fn content(&self) -> &Bytes {
+        &self.content
+    }
+
+    pub fn hash(&self) -> &Bytes {
+        &self.hash
+    }
+
+    pub fn as_publish(&self) -> PublishElement {
+        Publish::publish(&self.content, self.uri.clone())
+    }
+
+    pub fn as_update(&self, old_content: &Bytes) -> PublishElement {
+        Update::publish(old_content, &self.content, self.uri.clone())
+    }
+
+    pub fn as_withdraw(&self) -> PublishElement {
+        Withdraw::publish(&self.content, self.uri.clone())
+    }
+}
+
+impl PartialEq for CurrentFile {
+    fn eq(&self, other: &CurrentFile) -> bool {
+        self.uri == other.uri &&
+        self.hash == other.hash &&
+        self.content == other.content
+    }
+}
+
+impl Eq for CurrentFile {}
 
 
 //------------ Error ---------------------------------------------------------
@@ -129,6 +313,24 @@ pub enum Error {
 
     #[fail(display="{}", _0)]
     KeyStoreError(keystore::Error),
+
+    #[fail(display="Unknown publisher: {}", _0)]
+    UnknownPublisher(String),
+
+    #[fail(display="File already exists for uri (use update!): {}", _0)]
+    PublishHasExistingUri(uri::Rsync),
+
+    #[fail(display="File sent for update has no entry for uri: {}", _0)]
+    UpdateWrongUri(uri::Rsync),
+
+    #[fail(display="File for update exists, but hash does not match")]
+    UpdateWrongHash,
+
+    #[fail(display="The withdraw URI is not known: {}", _0)]
+    WithdrawWrongUri(uri::Rsync),
+
+    #[fail(display="File for withdraw exists, but hash does not match")]
+    WithdrawWrongHash,
 }
 
 impl From<io::Error> for Error {
@@ -171,7 +373,73 @@ mod tests {
             let publishers = repo.publishers().unwrap();
             assert_eq!(0, publishers.len());
         })
+    }
 
+    #[test]
+    fn should_add_list_remove_files() {
+        test::test_with_tmp_dir(|d| {
+
+            let mut repo = Repository::new(&d).unwrap();
+            let alice = RepoPublisher::new(
+                "alice".to_string(),
+                test::rsync_uri("rsync://host/module/alice/")
+            );
+
+            repo.add_publisher(alice).unwrap();
+
+            let alice = repo.get_publisher("alice").unwrap();
+            let files = alice.current_files();
+            assert_eq!(0, files.len());
+
+            let file = CurrentFile::new(
+                test::rsync_uri("rsync://host/module/alice/file.txt"),
+                Bytes::from("example content")
+            );
+
+            //--------- Add a single file
+            let mut builder = PublishQuery::build();
+            builder.add(file.clone().as_publish());
+            let message = builder.build_message();
+            let publish = message.as_query().unwrap().as_publish().unwrap();
+
+            repo.publish("alice", publish.clone()).unwrap();
+
+            let alice = repo.get_publisher("alice").unwrap();
+            let files = alice.current_files();
+            assert_eq!(1, files.len());
+
+            // Can't publish the same thing again, should then update!
+            assert!(repo.publish("alice", publish).is_err());
+
+            //--------- Update a single file
+            let file_update = CurrentFile::new(
+                file.uri().clone(),
+                Bytes::from("example updated content")
+            );
+
+            let mut builder = PublishQuery::build();
+            builder.add(file_update.clone().as_update(file.content()));
+            let message = builder.build_message();
+            let publish = message.as_query().unwrap().as_publish().unwrap();
+
+            repo.publish("alice", publish.clone()).unwrap();
+
+            let alice = repo.get_publisher("alice").unwrap();
+            let files = alice.current_files();
+            assert_eq!(1, files.len());
+
+            //--------- Withdraw a single file
+            let mut builder = PublishQuery::build();
+            builder.add(file_update.as_withdraw());
+            let message = builder.build_message();
+            let publish = message.as_query().unwrap().as_publish().unwrap();
+
+            repo.publish("alice", publish.clone()).unwrap();
+
+            let alice = repo.get_publisher("alice").unwrap();
+            let files = alice.current_files();
+            assert_eq!(0, files.len());
+        });
     }
 
 }
