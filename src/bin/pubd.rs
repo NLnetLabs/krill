83f218522901c4d3e82c63be171c89842ab6cab4
Tim Bruijnzeels <tim@nlnetlabs.nl>
2019-01-02 09:51:23 +0100

    Cleanup

--- src/bin/pubd.rs
+++ src/bin/pubd.rs
@@ -1,24 +1,14 @@
-extern crate actix;
 extern crate rpubd;
-
-#[macro_use] extern crate lazy_static;
-extern crate core;
 
 use rpubd::pubd::config::Config;
 use rpubd::pubd::http::PubServerApp;
 
-lazy_static! {
-    static ref CONFIG: Config = {
-        match Config::create() {
-            Ok(c)  => c,
-            Err(e) => {
-                eprintln!("{}", e);
-                ::std::process::exit(1);
-            }
-        }
-    };
-}
-
 fn main() {
-    PubServerApp::run(&CONFIG)
+    match Config::create() {
+        Ok(config) => PubServerApp::run(&config),
+        Err(e) => {
+            eprintln!("{}", e);
+            ::std::process::exit(1);
+        }
+    }
 }
