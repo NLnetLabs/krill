06acc47876f9dd25603638ff156a8710b872661d
Tim Bruijnzeels <tim@nlnetlabs.nl>
2019-04-18 09:33:13 +0200

    Initialise TrustAnchor with resources *and* its key. (Issue: #12)

--- ca/src/trustanchor.rs
+++ ca/src/trustanchor.rs
@@ -1,6 +1,7 @@
 use resources::ResourceSet;
 use krill_commons::api::admin::CaHandle;
 use krill_commons::eventsourcing::{StoredEvent, SentCommand, CommandDetails, Aggregate};
+use krill_commons::util::softsigner::SignerKeyId;
 
 #[allow(dead_code)]
 const TA_NS: &str = "trustanchors";
@@ -16,17 +17,18 @@ pub type TrustAnchorInit = StoredEvent<TrustAnchorInitDetails>;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct TrustAnchorInitDetails {
-    resources: ResourceSet
+    resources: ResourceSet,
+    key: SignerKeyId
 }
 
 impl TrustAnchorInitDetails {
-    pub fn with_all_resources(handle: &CaHandle) -> TrustAnchorInit {
+    pub fn with_all_resources(handle: &CaHandle, key: SignerKeyId) -> TrustAnchorInit {
         let details = {
             let asns = "AS0-AS4294967295";
             let v4 = "0.0.0.0/0";
             let v6 = "::/0";
             let resources = ResourceSet::from_strs(asns, v4, v6).unwrap();
-            TrustAnchorInitDetails { resources }
+            TrustAnchorInitDetails { resources, key }
         };
 
         TrustAnchorInit::new(handle.as_ref(), 0, details)
@@ -60,7 +62,9 @@ impl CommandDetails for TrustAnchorCommandDetails {
 pub struct TrustAnchor {
     id: CaHandle,
     version: u64,
-    resources: ResourceSet
+
+    resources: ResourceSet,
+    key: SignerKeyId
 }
 
 impl Aggregate for TrustAnchor {
@@ -74,8 +78,9 @@ impl Aggregate for TrustAnchor {
         let id = CaHandle::from(id);
         let version = 1; // after applying init
         let resources = init.resources;
+        let key = init.key;
 
-        Ok(TrustAnchor { id, version, resources })
+        Ok(TrustAnchor { id, version, resources, key })
     }
 
     fn version(&self) -> u64 {
@@ -105,16 +110,29 @@ impl std::error::Error for Error {}
 mod tests {
 
     use super::*;
+    use std::path::PathBuf;
     use krill_commons::util::test;
     use krill_commons::eventsourcing::AggregateStore;
     use krill_commons::eventsourcing::DiskAggregateStore;
+    use krill_commons::util::softsigner::OpenSslSigner;
+    use rpki::crypto::signer::Signer;
+    use rpki::crypto::PublicKeyFormat;
+
+    fn signer(temp_dir: &PathBuf) -> OpenSslSigner {
+        let signer_dir = test::create_sub_dir(temp_dir);
+        OpenSslSigner::build(&signer_dir).unwrap()
+    }
 
     #[test]
     fn should_init_ta() {
         test::test_with_tmp_dir(|d| {
             let store = DiskAggregateStore::<TrustAnchor>::new(&d, TA_NS).unwrap();
             let handle = ta_handle();
-            let init = TrustAnchorInitDetails::with_all_resources(&handle);
+
+            let mut signer = signer(&d);
+            let key = signer.create_key(PublicKeyFormat).unwrap();
+
+            let init = TrustAnchorInitDetails::with_all_resources(&handle, key);
 
             store.add(handle.as_ref(), init).unwrap();
         });
