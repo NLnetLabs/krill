643da3fb9c9be0ff2789a60b1e4844f20b154abf
Tim Bruijnzeels <tim@nlnetlabs.nl>
2019-09-16 12:19:02 -0700

    Introduce KRILL_USE_TA env variable for easier testing without the need for a local config file.

--- src/daemon/config.rs
+++ src/daemon/config.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io;
 use std::io::Read;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::env;
 use std::path::PathBuf;
 use std::str::FromStr;
 
@@ -32,7 +33,12 @@ impl ConfigDefaults {
         3000
     }
     fn use_ta() -> bool {
-        false
+
+        if let Ok(_) = env::var("KRILL_USE_TA") {
+            true
+        } else {
+            false
+        }
     }
     fn use_ssl() -> SslChoice {
         SslChoice::Test
@@ -59,7 +65,6 @@ impl ConfigDefaults {
         PathBuf::from("./krill.log")
     }
     fn auth_token() -> Token {
-        use std::env;
 
         match env::var("KRILL_AUTH_TOKEN") {
             Ok(token) => Token::from(token),
@@ -229,7 +234,7 @@ impl Config {
 
         let config_file = matches
             .value_of("config")
-            .unwrap_or("./daemon/defaults/krill.conf");
+            .unwrap_or("./defaults/krill.conf");
 
         let c = Self::read_config(config_file)?;
         c.init_logging()?;
