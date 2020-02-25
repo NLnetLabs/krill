75d4da900046ee9f1b8fa9a60a85b304f189e3e7
Tim Bruijnzeels <tim@nlnetlabs.nl>
2020-02-25 16:09:55 +0100

    Clean up dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,22 @@
-extern crate clap;
-#[macro_use]
-extern crate derive_more;
-extern crate rpki;
-#[macro_use]
-extern crate serde;
 extern crate base64;
 #[macro_use]
 extern crate bcder;
 extern crate bytes;
 extern crate chrono;
-extern crate futures;
+extern crate clap;
+extern crate clokwerk;
+#[macro_use]
+extern crate derive_more;
 extern crate hex;
+extern crate hyper;
 #[macro_use]
 extern crate log;
-// extern crate actix_service;
-// extern crate actix_web;
-extern crate clokwerk;
-extern crate hyper;
 extern crate openssl;
 extern crate rand;
 extern crate reqwest;
+extern crate rpki;
+#[macro_use]
+extern crate serde;
 extern crate serde_json;
 extern crate syslog;
 extern crate tokio;
