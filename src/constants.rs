3e517c0fa8340af59ac562ac7ec27ffe223d0257
Tim Bruijnzeels <tim@nlnetlabs.nl>
2020-06-28 22:57:50 +0200

    Update version to 0.7.2

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub const KRILL_VERSION: &str = "0.8.0-dev";
+pub const KRILL_VERSION: &str = "0.7.2";
 pub const KRILL_SERVER_APP: &str = "Krill";
 pub const KRILL_CLIENT_APP: &str = "Krill Client";
 
