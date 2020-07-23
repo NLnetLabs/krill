b34e687d881040928dd9f8d193c57cf3d0a7764a
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2020-07-23 16:55:34 +0200

    Bump Krill version for test purposes to be able to distinguish the old vs the new package post upgrade. (revert before merge)

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub const KRILL_VERSION: &str = "0.7.3";
+pub const KRILL_VERSION: &str = "0.7.3-test.1";
 pub const KRILL_SERVER_APP: &str = "Krill";
 pub const KRILL_CLIENT_APP: &str = "Krill Client";
 
