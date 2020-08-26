09132a0611c4f5b93cf8c456fc93af8f894d9d25
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2020-08-26 15:40:57 +0200

    FIX: broken test due to added  testbed CA.

--- tests/ca_embedded.rs
+++ tests/ca_embedded.rs
@@ -5,6 +5,7 @@ use std::str::FromStr;
 
 use krill::commons::api::{Handle, ParentCaReq, ResourceSet};
 use krill::daemon::ca::ta_handle;
+use krill::daemon::config::CONFIG;
 use krill::test::*;
 
 #[tokio::test]
@@ -18,6 +19,8 @@ async fn ca_embedded() {
 
     init_child_with_embedded_repo(&child).await;
 
+    let base_cert_count = if CONFIG.testbed_enabled { 1 } else { 0 };
+
     // Embedded parent --------------------------------------------------------------------
     let parent = {
         let parent_contact = add_child_to_ta_embedded(&child, child_resources.clone()).await;
@@ -27,13 +30,13 @@ async fn ca_embedded() {
     // When the parent is added, a child CA will immediately request a certificate.
     add_parent_to_ca(&child, parent).await;
     assert!(ca_gets_resources(&child, &child_resources).await);
-    assert!(ta_will_have_issued_n_certs(1).await);
+    assert!(ta_will_have_issued_n_certs(base_cert_count+1).await);
 
     // When the parent adds resources to a CA, it can request a new resource certificate.
     let new_child_resources = ResourceSet::from_strs("AS65000", "10.0.0.0/16", "").unwrap();
     update_child(&ta_handle, &child, &new_child_resources).await;
     assert!(ca_gets_resources(&child, &new_child_resources).await);
-    assert!(ta_will_have_issued_n_certs(1).await);
+    assert!(ta_will_have_issued_n_certs(base_cert_count+1).await);
 
     // When the removes child resources, the child will get a reduced certificate when it syncs.
     let child_resources = ResourceSet::from_strs("", "10.0.0.0/24", "").unwrap();
@@ -45,7 +48,7 @@ async fn ca_embedded() {
     let child_resources = ResourceSet::default();
     update_child(&ta_handle, &child, &child_resources).await;
     assert!(rc_is_removed(&child).await);
-    assert!(ta_will_have_issued_n_certs(0).await);
+    assert!(ta_will_have_issued_n_certs(base_cert_count).await);
 
     let _ = fs::remove_dir_all(dir);
 }
