a42fa5b45bc83c6b478e1d7716b9bed8a082fdcf
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2020-10-26 16:44:18 +0100

    Two fixes for API token based login: wrong login URL returned by Krill, and a JS error in Lagosta (fixed in Lagosta commit 0eaa44b8).

--- src/daemon/auth/providers/master_token.rs
+++ src/daemon/auth/providers/master_token.rs
@@ -3,7 +3,13 @@ use crate::commons::api::Token;
 use crate::commons::error::Error as KrillError;
 use crate::daemon::auth::{Auth, AuthProvider, LoggedInUser, Permissions};
 use crate::daemon::config::CONFIG;
-use crate::daemon::http::auth::AUTH_LOGIN_ENDPOINT;
+
+// This is NOT an actual relative path to redirect to. Instead it is the path
+// string of an entry in the Vue router routes table to "route" to (in the
+// Lagosta single page application). See the routes array in router.js of the
+// Lagosta source code. Ideally we could instead return a route name and then
+// Lagosta could change this path without requiring that we update to match.
+const LAGOSTA_LOGIN_ROUTE_PATH: &str = "/login";
 
 pub struct MasterTokenAuthProvider {
     token: Token,
@@ -27,7 +33,7 @@ impl AuthProvider for MasterTokenAuthProvider {
 
     fn get_login_url(&self) -> String {
         // Direct Lagosta to show the user the Lagosta API token login form
-        return AUTH_LOGIN_ENDPOINT.to_string();
+        return LAGOSTA_LOGIN_ROUTE_PATH.to_string();
     }
 
     fn login(&self, auth: &Auth) -> KrillResult<LoggedInUser> {
