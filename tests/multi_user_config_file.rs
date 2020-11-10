9dbf4c27eeb4be205900595a6e7157486ec2d733
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2020-11-10 12:51:59 +0100

    Be consistent in the cypress test setup, use "ui" everywhere, not "web-ui".

--- tests/multi_user_config_file.rs
+++ tests/multi_user_config_file.rs
@@ -1,7 +1,7 @@
 mod ui;
 
 #[test]
-#[cfg_attr(not(feature = "web-ui-tests"), ignore)]
+#[cfg_attr(not(feature = "ui-tests"), ignore)]
 fn multi_user_config_file_test() -> Result<(), Box<dyn std::error::Error>> {
     ui::run_krill_ui_test("multi_user_config_file")
 }
