ac41149c33a3b8a2e2c27b43823be02c5a55fe12
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2020-11-16 11:14:58 +0100

    Reduce test logic duplication and use of hard-coded values via use of config arrays.

--- tests/ui/cypress_specs/multi_user_config_file.js
+++ tests/ui/cypress_specs/multi_user_config_file.js
@@ -1,3 +1,17 @@
+let admin     = { u: 'admin@krill',     p: 'admin_pass'     };
+let readonly  = { u: 'readonly@krill',  p: 'readonly_pass'  };
+let readwrite = { u: 'readwrite@krill', p: 'readwrite_pass' };
+let ca_name   = 'dummy-ca-name';
+
+let login_test_settings = [
+  { d: 'empty',        u: '',                   p: '',                   o: false },
+  { d: 'master token', u: 'dummy-master-token', p: 'dummy-master-token', o: false },
+  { d: 'incorrect',    u: 'wrong_user_name',    p: 'wrong_password',     o: false },
+  { d: 'admin',        u: admin.u,              p: admin.p,              o: true  },
+  { d: 'readonly',     u: readonly.u,           p: readonly.p,           o: true  },
+  { d: 'readwrite',    u: readwrite.u,          p: readwrite.p,          o: true  },
+];
+
 describe('Config File Users', () => {
   it('The correct login form is shown', () => {
     cy.visit('/')
@@ -15,47 +29,38 @@ describe('Config File Users', () => {
     cy.get(':password')
     cy.contains('Password')
   })
-  
-  it('Cannot login with empty password', () => {
-    cy.visit('/')
-    cy.contains('Username')
-    cy.contains('Password')
-    cy.get(':password').clear()
-    cy.contains('Sign In').click()
-    cy.contains('Please enter your password')
-  })
 
-  it('Cannot login with master token', () => {
-    cy.visit('/')
-    cy.contains('Username')
-    cy.contains('Password')
-    cy.get(':password').type('dummy-master-token')
-    cy.contains('Sign In').click()
-    cy.contains('Please enter your username')
-  })
+  login_test_settings.forEach(function (ts) {
+    it('Login with ' + ts.d + ' credentials should ' + (ts.o ? 'succeed' : 'fail'), () => {
+      cy.visit('/')
+      cy.contains('Username')
+      cy.contains('Password')
 
-  it('Cannot login with incorrect credentials', () => {
-    cy.visit('/')
-    cy.get('input[placeholder="Your username"]').type('wrong_user_name')
-    cy.get(':password').clear().type('wrong_password')
-    cy.contains('Sign In').click()
-    cy.contains('The credentials you specified are wrong')
-  })
+      cy.get('input[placeholder="Your username"]').clear()
+      cy.get(':password').clear()
 
-  it('Can login with admin credentials', () => {
-    cy.visit('/')
-    cy.get('input[placeholder="Your username"]').type('admin@krill')
-    cy.get(':password').type('admin_pass')
-    cy.contains('Sign In').click()
-    cy.contains('Logged in as: admin@krill')
+      if (ts.u != '') cy.get('input[placeholder="Your username"]').type(ts.u)
+      if (ts.p != '') cy.get(':password').type(ts.p)
+
+      cy.contains('Sign In').click()
+
+      if (ts.u == '') cy.contains('Please enter your username')
+      if (ts.p == '') cy.contains('Please enter your password')
+
+      if (ts.o) {
+        cy.contains('Logged in as: ' + ts.u)
+      } else {
+        cy.contains('Sign In')
+      }
+    })
   })
 
   it('Can logout', () => {
     cy.visit('/')
-    cy.get('input[placeholder="Your username"]').type('admin@krill')
-    cy.get(':password').type('admin_pass')
+    cy.get('input[placeholder="Your username"]').type(admin.u)
+    cy.get(':password').type(admin.p)
     cy.contains('Sign In').click()
-    cy.contains('Logged in as: admin@krill')
+    cy.contains('Logged in as: ' + admin.u)
     cy.get('.logout').click()
     cy.contains('Sign In')
   })
@@ -66,22 +71,22 @@ describe('Config File Users', () => {
 
     // login
     cy.visit('/')
-    cy.get('input[placeholder="Your username"]').type('admin@krill')
-    cy.get(':password').type('admin_pass')
+    cy.get('input[placeholder="Your username"]').type(admin.u)
+    cy.get(':password').type(admin.p)
     cy.contains('Sign In').click()
-    cy.contains('Logged in as: admin@krill')
+    cy.contains('Logged in as: ' + admin.u)
 
     // Skip ahead a minute and check that we are still logged in
     cy.tick(1*60*1000)
     cy.visit('/')
-    cy.contains('Logged in as: admin@krill')
+    cy.contains('Logged in as: ' + admin.u)
     cy.contains('Sign In').should('not.exist')
 
     // Skip ahead till just before the idle timeout and check that we are still
     // logged in.
     cy.tick(28*60*1000)
     cy.visit('/')
-    cy.contains('Logged in as: admin@krill')
+    cy.contains('Logged in as: ' + admin.u)
     cy.contains('Sign In').should('not.exist')
 
     // Skip ahead another 31 minutes to just beyond the UI 30 minute idle
@@ -94,23 +99,23 @@ describe('Config File Users', () => {
 
   it('Can login with readonly credentials', () => {
     cy.visit('/')
-    cy.get('input[placeholder="Your username"]').type('readonly@krill')
-    cy.get(':password').type('readonly_pass')
+    cy.get('input[placeholder="Your username"]').type(readonly.u)
+    cy.get(':password').type(readonly.p)
     cy.contains('Sign In').click()
-    cy.contains('Logged in as: readonly@krill')
+    cy.contains('Logged in as: ' + readonly.u)
   })
 
   it('Cannot create CA as readonly user', () => {
     cy.visit('/')
-    cy.get('input[placeholder="Your username"]').type('readonly@krill')
-    cy.get(':password').type('readonly_pass')
+    cy.get('input[placeholder="Your username"]').type(readonly.u)
+    cy.get(':password').type(readonly.p)
     cy.contains('Sign In').click()
-    cy.contains('Logged in as: readonly@krill')
+    cy.contains('Logged in as: ' + readonly.u)
     cy.contains('Welcome to Krill')
 
     // try to create a CA
     cy.contains('CA Handle')
-    cy.get('form input[type="text"]').type('dummy-ca-name')
+    cy.get('form input[type="text"]').type(ca_name)
     cy.contains('Create CA').click()
     cy.contains('OK').click()
 
@@ -119,31 +124,23 @@ describe('Config File Users', () => {
     cy.contains('Error')
   })
 
-  it('Can login with readwrite credentials', () => {
-    cy.visit('/')
-    cy.get('input[placeholder="Your username"]').type('readwrite@krill')
-    cy.get(':password').type('readwrite_pass')
-    cy.contains('Sign In').click()
-    cy.contains('Logged in as: readwrite@krill')
-  })
-
   it('Can create CA as readwrite user', () => {
     cy.visit('/')
-    cy.get('input[placeholder="Your username"]').type('readwrite@krill')
-    cy.get(':password').type('readwrite_pass')
+    cy.get('input[placeholder="Your username"]').type(readwrite.u)
+    cy.get(':password').type(readwrite.p)
     cy.contains('Sign In').click()
-    cy.contains('Logged in as: readwrite@krill')
+    cy.contains('Logged in as: ' + readwrite.u)
     cy.contains('Welcome to Krill')
 
     // try to create a CA
     cy.contains('CA Handle')
-    cy.get('form input[type="text"]').type('dummy-ca-name')
+    cy.get('form input[type="text"]').type(ca_name)
     cy.contains('Create CA').click()
     cy.contains('OK').click()
 
     // we're no longer on the welcome page and the CA name we created is visible
     // on the page
     cy.contains('Welcome to Krill').should('not.exist')
-    cy.contains('dummy-ca-name')
+    cy.contains(ca_name)
   })
 })
