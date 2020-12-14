f444ee43e268e7cc9f81a01bd692221816db85c9
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2020-12-14 13:30:23 +0100

    Remove unhelpful spam from logs.

--- src/daemon/http/server.rs
+++ src/daemon/http/server.rs
@@ -162,11 +162,8 @@ impl ResultLogger {
             Ok(response) => {
                 info!("{} {} {}", self.req_method, self.req_path, response.status());
 
-                if log_enabled!(log::Level::Trace) {
-                    match response.loggable() {
-                        false => trace!("Response body: <filtered>"),
-                        true  => trace!("Response body: {:?}", response.body()),
-                    }
+                if response.loggable() && log_enabled!(log::Level::Trace) {
+                    trace!("Response body: {:?}", response.body());
                 }
             }
             Err(err) => {
