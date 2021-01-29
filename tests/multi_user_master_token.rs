a99aedd9fbdc25b545ad02027940af4738aca09e
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-01-29 08:29:19 +0100

    cargo fmt.

--- tests/multi_user_master_token.rs
+++ tests/multi_user_master_token.rs
@@ -4,5 +4,10 @@ mod ui;
 #[tokio::test]
 #[cfg(feature = "ui-tests")]
 async fn multi_user_master_token_test() {
-    ui::run_krill_ui_test("multi_user_master_token", ui::OpenIDConnectMockMode::OIDCProviderWillNotBeStarted, false).await;
+    ui::run_krill_ui_test(
+        "multi_user_master_token",
+        ui::OpenIDConnectMockMode::OIDCProviderWillNotBeStarted,
+        false,
+    )
+    .await;
 }
--- tests/multi_user_team_policy.rs
+++ tests/multi_user_team_policy.rs
@@ -4,5 +4,10 @@ mod ui;
 #[tokio::test]
 #[cfg(all(feature = "ui-tests", feature = "multi-user"))]
 async fn multi_user_team_policy_test() {
-    ui::run_krill_ui_test("multi_user_team_policy", ui::OpenIDConnectMockMode::OIDCProviderWillNotBeStarted, false).await
+    ui::run_krill_ui_test(
+        "multi_user_team_policy",
+        ui::OpenIDConnectMockMode::OIDCProviderWillNotBeStarted,
+        false,
+    )
+    .await
 }
