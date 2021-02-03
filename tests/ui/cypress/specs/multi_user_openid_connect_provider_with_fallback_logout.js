0791f56f9cd9375cc53cad1e504a4771361bf8ae
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-02-03 20:39:25 +0100

    Make the fallback logout URL test more deterministic.

--- tests/ui/cypress/specs/multi_user_openid_connect_provider_with_fallback_logout.js
+++ tests/ui/cypress/specs/multi_user_openid_connect_provider_with_fallback_logout.js
@@ -20,9 +20,11 @@ describe('OpenID Connect provider with OAuth 2 revocation', () => {
     cy.request('http://127.0.0.1:1818/test/is_user_logged_in?username=' + username).its('status').should('eq', 200)
 
     // logout
+    cy.intercept('/auth/logout').as('getLogoutURL')
     cy.get('.logout').click()
 
     // verify that we are shown the OpenID Connect provider login page
+    cy.wait('@getLogoutURL').its('response.statusCode').should('eq', 200)
     cy.url().should('not.include', Cypress.config('baseUrl'))
     cy.contains('Mock OpenID Connect login form')
     cy.get('input[name="username"]')
