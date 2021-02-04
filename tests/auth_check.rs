f5411c652eed886b63a97b6f525a83365ac88328
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-02-04 16:38:45 +0100

    Add a test that should (but doesn't in non-multi-user mode) panic because it tries to create a CA using the wrong master token.

---
+++ tests/auth_check.rs
@@ -0,0 +1,18 @@
+use std::str::FromStr;
+
+use krill::{commons::api::{Handle, Token}, test::{init_ca, start_krill, test_config, tmp_dir}};
+
+extern crate krill;
+
+#[tokio::test]
+#[should_panic]
+async fn auth_check() {
+    let dir = tmp_dir();
+    let mut config = test_config(&dir);
+    config.auth_token = Token::from("wrong secret");
+
+    start_krill(Some(config), false).await;
+
+    let ca_handle = Handle::from_str("dummy_ca").unwrap();
+    init_ca(&ca_handle).await;
+}
