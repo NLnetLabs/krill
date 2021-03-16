01521eb662d7dfaaa2586c9b2f42e0268e54f1cd
Tim Bruijnzeels <tim@nlnetlabs.nl>
2021-03-16 10:32:48 +0100

    Rename pubserver module directory.

--- src/pubd/mod.rs
+++ src/pubd/mod.rs
@@ -1,7 +1,7 @@
 mod commands;
 mod events;
+mod manager;
 mod publishers;
-mod pubserver;
 mod repository;
 
 pub use self::commands::{RepoAccessCmd, RepoAccessCmdDet};
@@ -9,6 +9,6 @@ pub use self::events::{
     RepositoryAccessEvent, RepositoryAccessEventDetails, RepositoryAccessIni, RepositoryAccessInitDetails,
     RrdpSessionReset, RrdpUpdate,
 };
+pub use self::manager::RepositoryManager;
 pub use self::publishers::Publisher;
-pub use self::pubserver::RepositoryManager;
 pub use self::repository::*;
