3d25be49966079c374f0d4cbe8b0b1385d6bf692
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-04-01 22:12:30 +0200

    FIX: typo.

--- tests/multi_user_openid_connect.rs
+++ tests/multi_user_openid_connect.rs
@@ -8,7 +8,7 @@ async fn multi_user_openid_connect_test() {
 
     ui::run_krill_ui_test(
         "multi_user_openid_connect",
-        OpenIDConnectMockConfig::enabed(WithRPInitiatedLogout)
+        OpenIDConnectMockConfig::enabled(WithRPInitiatedLogout)
     )
     .await
 }
