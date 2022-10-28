abbc9c6c49d7d55f3f1f27618ba527ea4bef9b20
Tim Bruijnzeels <tim@nlnetlabs.nl>
2022-10-28 15:06:48 +0200

    Use new krill-ui - still under development.

--- src/daemon/http/statics.rs
+++ src/daemon/http/statics.rs
@@ -14,50 +14,35 @@ pub async fn statics(req: Request) -> RoutingResult {
                     .unwrap(),
             )),
             "/index.html" => Ok(HttpResponse::html(INDEX)),
-            "/favicon.ico" => Ok(HttpResponse::fav(FAVICON)),
-            "/js/app.js" => Ok(HttpResponse::js(APP_JS)),
-            "/js/app.js.map" => Ok(HttpResponse::js(APP_JS_MAP)),
-            "/css/app.css" => Ok(HttpResponse::css(APP_CSS)),
-            "/img/krill_logo_white.svg" => Ok(HttpResponse::svg(IMG_KRILL_LOG)),
-            "/img/route_left.svg" => Ok(HttpResponse::svg(IMG_ROUTE_LEFT)),
-            "/img/route_right.svg" => Ok(HttpResponse::svg(IMG_ROUTE_RIGHT)),
-            "/img/welcome.svg" => Ok(HttpResponse::svg(IMG_ROUTE_WELCOME)),
-            "/fonts/element-icons.ttf" => Ok(HttpResponse::woff(FONTS_EL_ICONS_TTF)),
-            "/fonts/element-icons.woff" => Ok(HttpResponse::woff(FONTS_EL_ICONS)),
-            "/fonts/lato-latin-100.woff" => Ok(HttpResponse::woff(FONTS_LATIN_100)),
-            "/fonts/lato-latin-100.woff2" => Ok(HttpResponse::woff2(FONTS_LATIN_100_2)),
-            "/fonts/lato-latin-100italic.woff" => Ok(HttpResponse::woff(FONTS_LATIN_100_IT)),
-            "/fonts/lato-latin-100italic.woff2" => Ok(HttpResponse::woff2(FONTS_LATIN_100_IT_2)),
-            "/fonts/lato-latin-300.woff" => Ok(HttpResponse::woff(FONTS_LATIN_300)),
-            "/fonts/lato-latin-300.woff2" => Ok(HttpResponse::woff2(FONTS_LATIN_300_2)),
-            "/fonts/lato-latin-300italic.woff" => Ok(HttpResponse::woff(FONTS_LATIN_300_IT)),
-            "/fonts/lato-latin-300italic.woff2" => Ok(HttpResponse::woff2(FONTS_LATIN_300_IT_2)),
-            "/fonts/lato-latin-400.woff" => Ok(HttpResponse::woff(FONTS_LATIN_400)),
-            "/fonts/lato-latin-400.woff2" => Ok(HttpResponse::woff2(FONTS_LATIN_400_2)),
-            "/fonts/lato-latin-400italic.woff" => Ok(HttpResponse::woff(FONTS_LATIN_400_IT)),
-            "/fonts/lato-latin-400italic.woff2" => Ok(HttpResponse::woff2(FONTS_LATIN_400_IT_2)),
-            "/fonts/lato-latin-700.woff" => Ok(HttpResponse::woff(FONTS_LATIN_700)),
-            "/fonts/lato-latin-700.woff2" => Ok(HttpResponse::woff2(FONTS_LATIN_700_2)),
-            "/fonts/lato-latin-700italic.woff" => Ok(HttpResponse::woff(FONTS_LATIN_700_IT)),
-            "/fonts/lato-latin-700italic.woff2" => Ok(HttpResponse::woff2(FONTS_LATIN_700_IT_2)),
-            "/fonts/lato-latin-900.woff" => Ok(HttpResponse::woff(FONTS_LATIN_900)),
-            "/fonts/lato-latin-900.woff2" => Ok(HttpResponse::woff2(FONTS_LATIN_900_2)),
-            "/fonts/lato-latin-900italic.woff" => Ok(HttpResponse::woff(FONTS_LATIN_900_IT)),
-            "/fonts/lato-latin-900italic.woff2" => Ok(HttpResponse::woff2(FONTS_LATIN_900_IT_2)),
-            "/fonts/source-code-pro-latin-200.woff" => Ok(HttpResponse::woff(FONTS_SOURCE_CODE_200)),
-            "/fonts/source-code-pro-latin-200.woff2" => Ok(HttpResponse::woff2(FONTS_SOURCE_CODE_200_2)),
-            "/fonts/source-code-pro-latin-300.woff" => Ok(HttpResponse::woff(FONTS_SOURCE_CODE_300)),
-            "/fonts/source-code-pro-latin-300.woff2" => Ok(HttpResponse::woff2(FONTS_SOURCE_CODE_300_2)),
-            "/fonts/source-code-pro-latin-400.woff" => Ok(HttpResponse::woff(FONTS_SOURCE_CODE_400)),
-            "/fonts/source-code-pro-latin-400.woff2" => Ok(HttpResponse::woff2(FONTS_SOURCE_CODE_400_2)),
-            "/fonts/source-code-pro-latin-500.woff" => Ok(HttpResponse::woff(FONTS_SOURCE_CODE_500)),
-            "/fonts/source-code-pro-latin-500.woff2" => Ok(HttpResponse::woff2(FONTS_SOURCE_CODE_500_2)),
-            "/fonts/source-code-pro-latin-600.woff" => Ok(HttpResponse::woff(FONTS_SOURCE_CODE_600)),
-            "/fonts/source-code-pro-latin-600.woff2" => Ok(HttpResponse::woff2(FONTS_SOURCE_CODE_600_2)),
-            "/fonts/source-code-pro-latin-700.woff" => Ok(HttpResponse::woff(FONTS_SOURCE_CODE_700)),
-            "/fonts/source-code-pro-latin-700.woff2" => Ok(HttpResponse::woff2(FONTS_SOURCE_CODE_700_2)),
-            "/fonts/source-code-pro-latin-900.woff" => Ok(HttpResponse::woff(FONTS_SOURCE_CODE_900)),
-            "/fonts/source-code-pro-latin-900.woff2" => Ok(HttpResponse::woff2(FONTS_SOURCE_CODE_900_2)),
+
+            "/assets/favicon.f84116cb.ico" => Ok(HttpResponse::fav(FAVICON)),
+
+            "/assets/index.c4081ede.js" => Ok(HttpResponse::js(JS_INDEX)),
+
+            "/assets/en.225f43d6.js" => Ok(HttpResponse::js(JS_TRANSLATIONS_ENGLISH)),
+            "/assets/de.d29c510b.js" => Ok(HttpResponse::js(JS_TRANSLATIONS_GERMAN)),
+            "/assets/es.7aa2aa17.js" => Ok(HttpResponse::js(JS_TRANSLATIONS_SPANISH)),
+            "/assets/fr.6ea7925b.js" => Ok(HttpResponse::js(JS_TRANSLATIONS_FRENCH)),
+            "/assets/gr.f1bddb8f.js" => Ok(HttpResponse::js(JS_TRANSLATIONS_GREEK)),
+            "/assets/nl.2bcc2306.js" => Ok(HttpResponse::js(JS_TRANSLATIONS_DUTCH)),
+            "/assets/pt.e5846b3c.js" => Ok(HttpResponse::js(JS_TRANSLATIONS_PORTUGUESE)),
+
+            "/assets/index.5bd44b44.css" => Ok(HttpResponse::css(CSS)),
+
+            "/assets/check.3e734f78.svg" => Ok(HttpResponse::svg(SVG_CHECK)),
+            "/assets/error.fd1fc7e1.svg" => Ok(HttpResponse::svg(SVG_ERROR)),
+            "/assets/krill_logo_white.05224433.svg" => Ok(HttpResponse::svg(SVG_KRILL_LOGO)),
+            "/assets/logout.c725fd2c.svg" => Ok(HttpResponse::svg(SVG_LOGOUT)),
+            "/assets/plus-light.ec5e3c02.svg" => Ok(HttpResponse::svg(SVG_PLUS_LIGHT)),
+            "/assets/route-left.c88b44cb.svg" => Ok(HttpResponse::svg(SVG_ROUTE_LEFT)),
+            "/assets/route-right.17b0c46a.svg" => Ok(HttpResponse::svg(SVG_ROUTE_RIGHT)),
+            "/assets/search.4a30d812.svg" => Ok(HttpResponse::svg(SVG_SEARCH)),
+            "/assets/trash-can-light.d9c6ee55.svg" => Ok(HttpResponse::svg(SVG_TRASH_CAN)),
+            "/assets/user.5d1f1b14.svg" => Ok(HttpResponse::svg(SVG_USER)),
+
+            "/assets/Inter-italic.var.d1401419.woff2" => Ok(HttpResponse::woff2(FONTS_ITALIC)),
+            "/assets/Inter-roman.var.17fe38ab.woff2" => Ok(HttpResponse::woff2(FONTS_ROMAN)),
+
             _ => Err(req),
         },
         _ => Err(req),
@@ -72,54 +57,32 @@ pub async fn statics(req: Request) -> RoutingResult {
     })
 }
 
-static INDEX: &[u8] = include_bytes!("../../../lagosta/index.html");
-static FAVICON: &[u8] = include_bytes!("../../../lagosta/favicon.ico");
+static INDEX: &[u8] = include_bytes!("../../../ui/index.html");
 
-static APP_JS: &[u8] = include_bytes!("../../../lagosta/js/app.js");
-static APP_JS_MAP: &[u8] = include_bytes!("../../../lagosta/js/app.js.map");
+static FAVICON: &[u8] = include_bytes!("../../../ui/assets/favicon.f84116cb.ico");
 
-static APP_CSS: &[u8] = include_bytes!("../../../lagosta/css/app.css");
+static JS_INDEX: &[u8] = include_bytes!("../../../ui/assets/index.c4081ede.js");
 
-static IMG_KRILL_LOG: &[u8] = include_bytes!("../../../lagosta/img/krill_logo_white.svg");
-static IMG_ROUTE_LEFT: &[u8] = include_bytes!("../../../lagosta/img/route_left.svg");
-static IMG_ROUTE_RIGHT: &[u8] = include_bytes!("../../../lagosta/img/route_right.svg");
-static IMG_ROUTE_WELCOME: &[u8] = include_bytes!("../../../lagosta/img/welcome.svg");
+static JS_TRANSLATIONS_GERMAN: &[u8] = include_bytes!("../../../ui/assets/de.d29c510b.js");
+static JS_TRANSLATIONS_ENGLISH: &[u8] = include_bytes!("../../../ui/assets/en.225f43d6.js");
+static JS_TRANSLATIONS_SPANISH: &[u8] = include_bytes!("../../../ui/assets/es.7aa2aa17.js");
+static JS_TRANSLATIONS_FRENCH: &[u8] = include_bytes!("../../../ui/assets/fr.6ea7925b.js");
+static JS_TRANSLATIONS_GREEK: &[u8] = include_bytes!("../../../ui/assets/gr.f1bddb8f.js");
+static JS_TRANSLATIONS_DUTCH: &[u8] = include_bytes!("../../../ui/assets/nl.2bcc2306.js");
+static JS_TRANSLATIONS_PORTUGUESE: &[u8] = include_bytes!("../../../ui/assets/pt.e5846b3c.js");
 
-static FONTS_EL_ICONS_TTF: &[u8] = include_bytes!("../../../lagosta/fonts/element-icons.ttf");
-static FONTS_EL_ICONS: &[u8] = include_bytes!("../../../lagosta/fonts/element-icons.woff");
-static FONTS_LATIN_100: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-100.woff");
-static FONTS_LATIN_100_2: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-100.woff2");
-static FONTS_LATIN_100_IT: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-100italic.woff");
-static FONTS_LATIN_100_IT_2: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-100italic.woff2");
-static FONTS_LATIN_300: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-300.woff");
-static FONTS_LATIN_300_2: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-300.woff2");
-static FONTS_LATIN_300_IT: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-300italic.woff");
-static FONTS_LATIN_300_IT_2: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-300italic.woff2");
-static FONTS_LATIN_400: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-400.woff");
-static FONTS_LATIN_400_2: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-400.woff2");
-static FONTS_LATIN_400_IT: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-400italic.woff");
+static CSS: &[u8] = include_bytes!("../../../ui/assets/index.5bd44b44.css");
 
-static FONTS_LATIN_400_IT_2: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-400italic.woff2");
-static FONTS_LATIN_700: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-700.woff");
-static FONTS_LATIN_700_2: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-700.woff2");
-static FONTS_LATIN_700_IT: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-700italic.woff");
-static FONTS_LATIN_700_IT_2: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-700italic.woff2");
-static FONTS_LATIN_900: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-900.woff");
-static FONTS_LATIN_900_2: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-900.woff2");
-static FONTS_LATIN_900_IT: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-900italic.woff");
-static FONTS_LATIN_900_IT_2: &[u8] = include_bytes!("../../../lagosta/fonts/lato-latin-900italic.woff2");
+static SVG_CHECK: &[u8] = include_bytes!("../../../ui/assets/check.3e734f78.svg");
+static SVG_ERROR: &[u8] = include_bytes!("../../../ui/assets/error.fd1fc7e1.svg");
+static SVG_KRILL_LOGO: &[u8] = include_bytes!("../../../ui/assets/krill_logo_white.05224433.svg");
+static SVG_LOGOUT: &[u8] = include_bytes!("../../../ui/assets/logout.c725fd2c.svg");
+static SVG_PLUS_LIGHT: &[u8] = include_bytes!("../../../ui/assets/plus-light.ec5e3c02.svg");
+static SVG_ROUTE_LEFT: &[u8] = include_bytes!("../../../ui/assets/route-left.c88b44cb.svg");
+static SVG_ROUTE_RIGHT: &[u8] = include_bytes!("../../../ui/assets/route-right.17b0c46a.svg");
+static SVG_SEARCH: &[u8] = include_bytes!("../../../ui/assets/search.4a30d812.svg");
+static SVG_TRASH_CAN: &[u8] = include_bytes!("../../../ui/assets/trash-can-light.d9c6ee55.svg");
+static SVG_USER: &[u8] = include_bytes!("../../../ui/assets/user.5d1f1b14.svg");
 
-static FONTS_SOURCE_CODE_200: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-200.woff");
-static FONTS_SOURCE_CODE_200_2: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-200.woff2");
-static FONTS_SOURCE_CODE_300: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-300.woff");
-static FONTS_SOURCE_CODE_300_2: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-300.woff2");
-static FONTS_SOURCE_CODE_400: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-400.woff");
-static FONTS_SOURCE_CODE_400_2: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-400.woff2");
-static FONTS_SOURCE_CODE_500: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-500.woff");
-static FONTS_SOURCE_CODE_500_2: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-500.woff2");
-static FONTS_SOURCE_CODE_600: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-600.woff");
-static FONTS_SOURCE_CODE_600_2: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-600.woff2");
-static FONTS_SOURCE_CODE_700: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-700.woff");
-static FONTS_SOURCE_CODE_700_2: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-700.woff2");
-static FONTS_SOURCE_CODE_900: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-900.woff");
-static FONTS_SOURCE_CODE_900_2: &[u8] = include_bytes!("../../../lagosta/fonts/source-code-pro-latin-900.woff2");
+static FONTS_ITALIC: &[u8] = include_bytes!("../../../ui/assets/Inter-italic.var.d1401419.woff2");
+static FONTS_ROMAN: &[u8] = include_bytes!("../../../ui/assets/Inter-roman.var.17fe38ab.woff2");
