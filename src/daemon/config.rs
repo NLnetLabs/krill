99c5978182b91710ad128922b0151fcdd71d37de
ximon18 <3304436+ximon18@users.noreply.github.com>
2022-12-14 17:02:28 +0100

    Oops, Krill doesn't serve the rsync protocol so we can't specify the right port number to use nor is it the HTTP port.

--- src/daemon/config.rs
+++ src/daemon/config.rs
@@ -991,10 +991,10 @@ impl Config {
 
         let testbed = if enable_testbed {
             Some(TestBed::new(
-                test::rsync(&format!("rsync://localhost:{}/ta/ta.cer", port)),
+                test::rsync("rsync://localhost/ta/ta.cer"),
                 test::https(&format!("https://localhost:{}/ta/ta.cer", port)),
                 test::https(&format!("https://localhost:{}/rrdp/", port)),
-                test::rsync(&format!("rsync://localhost:{}/repo/", port)),
+                test::rsync("rsync://localhost/repo/"),
             ))
         } else {
             None
