4c455d4fc5860939b7e9a10eb2405b726b0e7304
ximon18 <3304436+ximon18@users.noreply.github.com>
2023-02-28 17:59:24 +0100

    Compilation fix.

--- src/commons/crypto/mod.rs
+++ src/commons/crypto/mod.rs
@@ -10,4 +10,5 @@ pub type SignerHandle = MyHandle;
 
 pub type CryptoResult<T> = std::result::Result<T, self::error::Error>;
 
+#[cfg(feature = "hsm")]
 pub use self::signers::pkcs11::signer::PubKeyAccess;
