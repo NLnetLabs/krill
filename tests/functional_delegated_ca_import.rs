1751273d641f0b5564ce174e8e742286fa12b004
Tim Bruijnzeels <tim@nlnetlabs.nl>
2023-10-13 16:37:25 +0200

    Test delegated CA import using a single Krill instance.

--- tests/functional_delegated_ca_import.rs
+++ tests/functional_delegated_ca_import.rs
@@ -45,49 +45,55 @@ async fn functional_delegated_ca_import() {
     // Update the child resources
 
     let testbed = ca_handle("testbed");
+    let parent_1 = ca_handle("parent_1");
+    let parent_2 = ca_handle("parent_2");
+
+    let parent_res = ResourceSet::all();
+
     let child = ca_handle("child");
     let child_res = resources("AS65000", "10.0.0.0/16", "");
     let child_res_2 = resources("AS65000-AS65010", "10.0.0.0/8", "2001:db8::/32");
     let child_rcn = ResourceClassName::from("custom");
 
     // Start a testbed
-    let testbed_1_clean = start_testbed(3000).await;
+    let clean = start_testbed(3000).await;
 
-    // Start a second testbed
-    let testbed_2_clean = start_testbed(3002).await;
+    // Add parent_1
+    set_up_ca_with_repo(&parent_1).await;
+    set_up_ca_under_parent(&parent_1, &testbed, &parent_res, None).await;
 
-    // Add child in testbed one
+    // Add child under parent_1
     set_up_ca_with_repo(&child).await;
-    set_up_ca_under_parent_main_krill(&child, &testbed, &child_res, child_rcn).await;
+    set_up_ca_under_parent(&child, &parent_1, &child_res, Some(child_rcn)).await;
 
     // Export the child
-    let exported_child = export_child_main_krill(&testbed, &child).await;
+    let exported_child = export_child(&parent_1, &child).await;
 
-    // Import child into testbed on the other server
-    import_child_secondary_krill(&testbed, exported_child).await;
+    // Add parent_2
+    set_up_ca_with_repo(&parent_2).await;
+    set_up_ca_under_parent(&parent_2, &testbed, &parent_res, None).await;
+
+    // Import child into parent_2
+    import_child(&parent_2, exported_child).await;
 
     // Add testbed in other server as parent to child
-    let response = parent_contact_secondary_krill(&testbed, &child).await;
-    let parent_ca_req = ParentCaReq::new(testbed.convert(), response);
+    let response = parent_contact(&parent_2, &child).await;
+    let parent_ca_req = ParentCaReq::new(parent_2.convert(), response);
     add_parent_to_ca(&child, parent_ca_req).await;
 
     // Remove the child from the original parent
-    delete_child(&testbed, &child).await;
+    delete_child(&parent_1, &child).await;
 
     // Update the resources for the child in the new
     // parent, then synchronise it, and verify that
     // the resources are received.
-    update_child_resources_secondary_krill(&testbed, &child, &child_res_2).await;
-
-    sleep_seconds(5).await; // because CI can be slow
-    cas_refresh_single(&child).await;
+    update_child_resources(&parent_2, &child, &child_res_2).await;
     assert!(ca_contains_resources(&child, &child_res_2).await);
 
-    testbed_1_clean();
-    testbed_2_clean();
+    clean();
 }
 
-async fn export_child_main_krill(parent: &CaHandle, child: &CaHandle) -> ExportChild {
+async fn export_child(parent: &CaHandle, child: &CaHandle) -> ExportChild {
     match krill_admin(Command::CertAuth(CaCommand::ChildExport(
         parent.clone(),
         child.convert(),
@@ -101,8 +107,8 @@ async fn export_child_main_krill(parent: &CaHandle, child: &CaHandle) -> ExportC
     }
 }
 
-async fn import_child_secondary_krill(parent: &CaHandle, child: ImportChild) {
-    match krill2_admin(Command::CertAuth(CaCommand::ChildImport(parent.clone(), child))).await {
+async fn import_child(parent: &CaHandle, child: ImportChild) {
+    match krill_admin(Command::CertAuth(CaCommand::ChildImport(parent.clone(), child))).await {
         ApiResponse::Empty => {}
         _ => {
             panic!("Expected exported child")
@@ -110,33 +116,36 @@ async fn import_child_secondary_krill(parent: &CaHandle, child: ImportChild) {
     }
 }
 
-async fn set_up_ca_under_parent_main_krill(
+async fn set_up_ca_under_parent(
     ca: &CaHandle,
     parent: &CaHandle,
     resources: &ResourceSet,
-    child_rcn: ResourceClassName,
+    child_rcn: Option<ResourceClassName>,
 ) {
     let child_request = request(ca).await;
     let parent_ca_req = {
         let response = add_child_rfc6492(parent.convert(), ca.convert(), child_request, resources.clone()).await;
         ParentCaReq::new(parent.convert(), response)
     };
-    let mapping = ResourceClassNameMapping {
-        name_in_parent: rcn(0),
-        name_for_child: child_rcn,
-    };
-    krill_admin(krill::cli::options::Command::CertAuth(CaCommand::ChildUpdate(
-        parent.convert(),
-        ca.convert(),
-        UpdateChildRequest::resource_class_name_mapping(mapping),
-    )))
-    .await;
+
+    if let Some(child_rcn) = child_rcn {
+        let mapping = ResourceClassNameMapping {
+            name_in_parent: rcn(0),
+            name_for_child: child_rcn,
+        };
+        krill_admin(krill::cli::options::Command::CertAuth(CaCommand::ChildUpdate(
+            parent.convert(),
+            ca.convert(),
+            UpdateChildRequest::resource_class_name_mapping(mapping),
+        )))
+        .await;
+    }
     add_parent_to_ca(ca, parent_ca_req).await;
     assert!(ca_contains_resources(ca, resources).await);
 }
 
-async fn parent_contact_secondary_krill(ca: &CaHandle, child: &CaHandle) -> ParentResponse {
-    match krill2_admin(Command::CertAuth(CaCommand::ParentResponse(
+async fn parent_contact(ca: &CaHandle, child: &CaHandle) -> ParentResponse {
+    match krill_admin(Command::CertAuth(CaCommand::ParentResponse(
         ca.clone(),
         child.convert(),
     )))
@@ -147,10 +156,10 @@ async fn parent_contact_secondary_krill(ca: &CaHandle, child: &CaHandle) -> Pare
     }
 }
 
-async fn update_child_resources_secondary_krill(ca: &CaHandle, child: &CaHandle, resources: &ResourceSet) {
+async fn update_child_resources(ca: &CaHandle, child: &CaHandle, resources: &ResourceSet) {
     let child_handle = child.convert();
     let req = UpdateChildRequest::resources(resources.clone());
-    match krill2_admin(Command::CertAuth(CaCommand::ChildUpdate(ca.clone(), child_handle, req))).await {
+    match krill_admin(Command::CertAuth(CaCommand::ChildUpdate(ca.clone(), child_handle, req))).await {
         ApiResponse::Empty => {}
         _ => panic!("Expected empty ok response"),
     }
