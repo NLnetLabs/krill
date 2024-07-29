97c0f26126517de50ed4318750c3f71b166dc2a6
Martin Hoffmann <martin@nlnetlabs.nl>
2024-07-29 14:08:52 +0200

    Bring back krillup.

--- src/bin/krillup.rs
+++ src/bin/krillup.rs
@@ -1,92 +1,161 @@
-/*
-extern crate krill;
+//! Krill data migration tool.
 
-use clap::{App, Arg, ArgMatches, SubCommand};
-use log::{info, LevelFilter};
-
-use krill::{
-    constants::{KRILL_DEFAULT_CONFIG_FILE, KRILL_UP_APP, KRILL_VERSION},
-    daemon::{
-        config::{Config, LogType},
-        properties::PropertiesManager,
-    },
-    upgrades::{
-        data_migration::migrate, prepare_upgrade_data_migrations, UpgradeMode,
-    },
-};
+use std::process;
+use std::path::PathBuf;
+use clap::Parser;
+use log::info;
+use log::LevelFilter;
 use url::Url;
-*/
+use krill::constants;
+use krill::daemon::config::{Config, LogType};
+use krill::daemon::properties::PropertiesManager;
+use krill::upgrades::{
+    data_migration::migrate, prepare_upgrade_data_migrations, UpgradeMode,
+};
+
+
+//------------ main ----------------------------------------------------------
 
 fn main() {
-/*
-    let matches = make_matches();
+    let options = Options::parse();
 
-    match parse_matches(matches) {
-        Err(e) => {
-            eprintln!("{}", e);
-            ::std::process::exit(1);
+    // Load config.
+    let mut config = match Config::create(&options.config, true) {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("Failed to read config file '{}': {}'",
+                options.config.display(), err
+            );
+            process::exit(1);
         }
-        Ok(mode) => match mode {
-            KrillUpMode::Prepare { config } => {
-                let properties_manager = match PropertiesManager::create(
-                    &config.storage_uri,
-                    config.use_history_cache,
-                ) {
-                    Ok(mgr) => mgr,
-                    Err(e) => {
-                        eprintln!("*** Error Preparing Data Migration ***");
-                        eprintln!("{}", e);
-                        eprintln!();
-                        eprintln!("Note that your server data has NOT been modified. Do not upgrade krill itself yet!");
-                        eprintln!("If you did upgrade krill, then downgrade it to your previous installed version.");
-                        ::std::process::exit(1);
-                    }
-                };
+    };
+    config.log_level = LevelFilter::Info;
+    config.log_type = LogType::Stderr;
 
-                match prepare_upgrade_data_migrations(
-                    UpgradeMode::PrepareOnly,
-                    &config,
-                    &properties_manager,
-                ) {
-                    Err(e) => {
-                        eprintln!("*** Error Preparing Data Migration ***");
-                        eprintln!("{}", e);
-                        eprintln!();
-                        eprintln!("Note that your server data has NOT been modified. Do not upgrade krill itself yet!");
-                        eprintln!("If you did upgrade krill, then downgrade it to your previous installed version.");
-                        ::std::process::exit(1);
-                    }
-                    Ok(opt) => match opt {
-                        None => {
-                            info!("No update needed");
-                        }
-                        Some(report) => {
-                            let from = report.versions().from();
-                            let to = report.versions().to();
-                            if report.data_migration() {
-                                info!("Prepared and verified upgrade from {} to {}.", from, to,);
-                            } else {
-                                info!("No preparation is needed for the upgrade from {} to {}.", from, to)
-                            }
-                        }
-                    },
-                }
-            }
-            KrillUpMode::Migrate { config, target } => {
-                if let Err(e) = migrate(config, target) {
-                    eprintln!("*** Error Migrating DATA ***");
+    match options.command {
+        Command::Prepare(_prepare) => {
+            let properties_manager = match PropertiesManager::create(
+                &config.storage_uri,
+                config.use_history_cache,
+            ) {
+                Ok(mgr) => mgr,
+                Err(e) => {
+                    eprintln!("*** Error Preparing Data Migration ***");
                     eprintln!("{}", e);
                     eprintln!();
                     eprintln!(
-                        "Note that your server data has NOT been modified."
+                        "Note that your server data has NOT been modified. \
+                         Do not upgrade krill"
+                    );
+                    eprintln!("itself yet!");
+                    eprintln!(
+                        "If you did upgrade krill, then downgrade it to \
+                         the previous installed version."
                     );
                     ::std::process::exit(1);
                 }
+            };
+
+            match prepare_upgrade_data_migrations(
+                UpgradeMode::PrepareOnly,
+                &config,
+                &properties_manager,
+            ) {
+                Err(e) => {
+                    eprintln!("*** Error Preparing Data Migration ***");
+                    eprintln!("{}", e);
+                    eprintln!();
+                    eprintln!(
+                        "Note that your server data has NOT been modified. \
+                         Do not upgrade krill"
+                    );
+                    eprintln!("itself yet!");
+                    eprintln!(
+                        "If you did upgrade krill, then downgrade it to \
+                         the previous installed version."
+                    );
+                    ::std::process::exit(1);
+                }
+                Ok(None) => info!("No update needed"),
+                Ok(Some(report)) => {
+                    let from = report.versions().from();
+                    let to = report.versions().to();
+                    if report.data_migration() {
+                        info!(
+                            "Prepared and verified upgrade from {} to {}.",
+                            from, to
+                        );
+                    } else {
+                        info!(
+                            "No preparation is needed for the upgrade from \
+                             {} to {}.",
+                            from, to
+                        )
+                    }
+                },
             }
-        },
+        }
+        Command::Migrate(cmd) => {
+            if let Err(e) = migrate(config, cmd.target) {
+                eprintln!("*** Error Migrating DATA ***");
+                eprintln!("{}", e);
+                eprintln!();
+                eprintln!(
+                    "Note that your server data has NOT been modified."
+                );
+                ::std::process::exit(1);
+            }
+        }
     }
 }
 
+
+//------------ Options -------------------------------------------------------
+
+/// The command line options for the krillup command.
+#[derive(clap::Parser)]
+pub struct Options {
+    /// Path to the Krill config file
+    #[arg(
+        short, long,
+        value_name = "path",
+        default_value = constants::KRILL_DEFAULT_CONFIG_FILE,
+    )]
+    pub config: PathBuf,
+
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+
+//------------ Command -------------------------------------------------------
+
+#[derive(clap::Subcommand)]
+pub enum Command {
+    /// Prepare a migration, leave current data unmodified
+    Prepare(Prepare),
+
+    /// Migrate data to different storage. Stop Krill before use!
+    Migrate(Migrate),
+}
+
+
+//------------ Prepare -------------------------------------------------------
+
+#[derive(clap::Args)]
+pub struct Prepare;
+
+
+//------------ Migrate -------------------------------------------------------
+
+#[derive(clap::Args)]
+pub struct Migrate {
+    /// The storage target as a URI string.
+    #[arg(short, long, value_name = "URI")]
+    pub target: Url,
+}
+
+/*
 fn make_matches<'a>() -> ArgMatches<'a> {
     let mut app = App::new(KRILL_UP_APP).version(KRILL_VERSION).about("\nThis tool can be used to reduce the risk and time needed for Krill upgrades, by preparing and verifying any data migrations that would be needed. The data_dir setting from the provided configuration file is used to find the data to migrate, and prepared data will be saved under 'data_dir/upgrade-data'.");
 
@@ -144,21 +213,8 @@ fn parse_matches(matches: ArgMatches) -> Result<KrillUpMode, String> {
     }
 }
 
-fn parse_config(m: &ArgMatches) -> Result<Config, String> {
-    let config_file =
-        m.value_of("config").unwrap_or(KRILL_DEFAULT_CONFIG_FILE);
-    let mut config = Config::create(config_file, true).map_err(|e| {
-        format!("Cannot parse config file '{}'. Error: {}", config_file, e)
-    })?;
-
-    config.log_level = LevelFilter::Info;
-    config.log_type = LogType::Stderr;
-
-    Ok(config)
-}
-
 enum KrillUpMode {
     Prepare { config: Config },
     Migrate { config: Config, target: Url },
-    */
 }
+    */
