089f0c981185c1a08b8beaea8d8352ed38067126
Martin Hoffmann <martin@nlnetlabs.nl>
2024-09-12 14:20:48 +0200

    Auth policies are now always available.

--- src/daemon/auth/mod.rs
+++ src/daemon/auth/mod.rs
@@ -3,22 +3,7 @@ pub mod providers;
 
 pub mod common;
 
-#[cfg(feature = "multi-user")]
 pub mod policy;
-#[cfg(not(feature = "multi-user"))]
-pub mod policy {
-    use std::sync::Arc;
-
-    use crate::{commons::KrillResult, daemon::config::Config};
-
-    #[derive(Clone)]
-    pub struct AuthPolicy {}
-    impl AuthPolicy {
-        pub fn new(_: Arc<Config>) -> KrillResult<Self> {
-            Ok(AuthPolicy {})
-        }
-    }
-}
 
 pub use authorizer::{
     Auth, AuthInfo, AuthProvider, Authorizer, Handle, LoggedInUser
