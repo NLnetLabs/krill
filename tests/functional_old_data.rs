67cb544ab00c009a3f32860089832896d3a5ff8d
Koen van Hove <koen@nlnetlabs.nl>
2025-06-12 16:13:58 +0200

    Add test to migrate from previous version (#1287)
    This test will try to run Krill with old data, to ensure structs that were
    used still work and do not crash Krill.

---
+++ tests/functional_old_data.rs
@@ -0,0 +1,69 @@
+//! Test setting up Krill as with data from the previous version.
+
+use std::{fs, path};
+use chrono::Datelike;
+use krill::cli::ta::signer::TrustAnchorSignerManager;
+
+mod common;
+
+
+fn untar_file(tar_path: &str, dst: impl AsRef<path::Path>) {
+    let mut archive = tar::Archive::new(
+        std::io::BufReader::new(
+            fs::File::open(tar_path).unwrap()
+        )
+    );
+    archive.unpack(dst).unwrap();
+}
+
+/// This function tests whether Krill in its current state still works with data
+/// from v0.14.5, even as a TA. If it does not, then we might have a problem.
+/// 
+/// The test data contains ROA, ASPA, BGPsec, and child objects.
+#[tokio::test]
+async fn functional_old_data() {
+    let (mut config, tempdir) = common::TestConfig::file_storage()
+        .enable_second_signer().finalize();
+
+    fs::create_dir(&tempdir.path().join("ta")).unwrap();
+    untar_file(
+        "test-resources/migrations/v0_14_5.tar", 
+        &tempdir.path().join("data")
+    );
+    untar_file(
+        "test-resources/migrations/v0_14_5_signer.tar", 
+        &tempdir.path().join("ta")
+    );
+    config.ta_support_enabled = true;
+
+    eprintln!(">>>> Check whether Krill still starts.");
+    let server = common::KrillServer::start_with_config(config).await;
+
+    let signer_config = 
+        include_str!("../test-resources/migrations/v0_14_5_signer/ta.conf");
+    let signer_config = signer_config.replace("%TEMPDIR%", 
+        &tempdir.path().join("ta").to_str().unwrap());
+
+    eprintln!(">>>> Configure the TA signer.");
+    let signer = TrustAnchorSignerManager::create(
+        krill::tasigner::Config::parse_str(
+            &signer_config
+        ).unwrap()
+    ).unwrap();
+
+    eprintln!(">>>> Make TA proxy signer request.");
+    let request = 
+        server.client().ta_proxy_signer_make_request().await.unwrap();
+    assert_eq!(request.renew_time.unwrap().year(), 2039);
+
+    eprintln!(">>>> Sign TA proxy signer request.");
+    let response = signer.process(request.into(), None).unwrap();
+    assert_eq!(response.content().child_responses.len(), 1);
+
+    eprintln!(">>>> Process TA proxy signer response.");
+    server.client().ta_proxy_signer_response(response).await.unwrap();
+
+    eprintln!(">>>> Fetch TAL and check it isn't empty.");
+    assert!(!server.client().testbed_tal().await.unwrap().is_empty());
+}
+
