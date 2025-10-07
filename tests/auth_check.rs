9bd6cbb0f6b89f1a804ac1e614dbb80dc379c7f8
Koen <koen@nlnetlabs.nl>
2025-10-07 14:34:12 +0200

    Add auth test too

--- tests/auth_check.rs
+++ tests/auth_check.rs
@@ -20,7 +20,47 @@ async fn auth_check() {
 
     // Now try and create a CA. This should fail with a “Forbidden” error.
     let res = client.ca_add(common::ca_handle("dummy_ca")).await;
-    dbg!(&res);
+    assert!(
+        matches!(
+            res,
+            Err(
+                httpclient::Error::ErrorResponseWithJson(
+                    _, StatusCode::UNAUTHORIZED, _
+                )
+                | httpclient::Error::Forbidden(_)
+            )
+        )
+    );
+}
+
+#[tokio::test]
+#[cfg(unix)]
+async fn auth_check_unix() {
+    use std::collections::HashMap;
+
+    use krill::cli::client::ServerUri;
+
+    let (mut config, _tempdir) = common::TestConfig::mem_storage()
+        .enable_testbed().enable_ca_refresh().finalize();
+
+    // The user that is executing the test gets read access to everything
+    let uid = nix::unistd::Uid::current();
+    let user = nix::unistd::User::from_uid(uid).unwrap().unwrap();
+    let file_sock = tempfile::NamedTempFile::new().unwrap();
+    config.unix_socket = Some(file_sock.path().into());
+    config.unix_users = HashMap::from([(user.name, "readonly".to_string())]);
+
+    let _server = common::KrillServer::start_with_config_unix(config).await;
+
+    let client = KrillClient::new(
+        ServerUri::try_from(
+            format!("unix://{}", file_sock.path().display())
+        ).unwrap(), 
+        None
+    ).unwrap();
+    
+    // Now try and create a CA. This should fail with a “Forbidden” error.
+    let res = client.ca_add(common::ca_handle("dummy_ca")).await;
     assert!(
         matches!(
             res,
