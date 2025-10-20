868e2fefe1bac521098cd475e04ae3bf2be1bf95
Koen <koen@nlnetlabs.nl>
2025-10-20 18:01:04 +0200

    Attempt to fix #1330

--- src/server/ca/manager.rs
+++ src/server/ca/manager.rs
@@ -800,9 +800,13 @@ impl CaManager {
             }
         }
 
-        self.ca_store.drop_aggregate(ca_handle)?;
+        if let Ok(_) = self.ca_store.get_latest(ca_handle) {
+            self.ca_store.drop_aggregate(ca_handle)?;
+        }
         self.ca_objects_store.remove_ca(ca_handle)?;
-        self.status_store.remove_ca(ca_handle)?;
+        if self.status_store.get_ca_status(ca_handle) != CaStatus::default() {
+            self.status_store.remove_ca(ca_handle)?;
+        }
 
         Ok(())
     }
